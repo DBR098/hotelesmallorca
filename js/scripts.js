2f32b8b71150ae85890b00d686577755159e7228
SATANAZI <dennybarreirorubio@hotmail.com>
2021-04-06 16:19:18 +0200

    ajax

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function prueba() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange == function() {
-        if(this.readyState == 4 && this.status == 1) {
-            document.getElementById("myCarousel") = this.responseText();
+        if(this.readyState == 4 && this.status == 200) {
+            document.getElementById("myCarousel").innerHTML = this.responseText();
         }
-    }
+    };
     xhttp.open("GET", "js/prueba.txt", true);
     xhttp.send();
 }
