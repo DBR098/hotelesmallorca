2db22ff598c0d8be2bdc845caf5a3cd7c62b85aa
CarlosSR021 <77896226+CarlosSR021@users.noreply.github.com>
2021-04-08 17:43:27 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function prueba() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange == function() {
+        alert('ESPAÑA');
         if(this.readyState == 4 && this.status == 200) {
             document.getElementById("myCarousel").innerHTML = this.responseText();
         }
