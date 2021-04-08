9e2b9ec6c7d6c70f76778ec0663be67ddfcda3b3
CarlosSR021 <77896226+CarlosSR021@users.noreply.github.com>
2021-04-08 18:23:31 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,16 @@ function prueba() {
     xhttp.open("GET", 'js/prueba.txt', true);
     xhttp.send(null);
 }
+
+function muestraOculta(id) {
+    var elemento = document.getElementById('contenidos_'+id);
+    var enlace = document.getElementById('enlace_'+id);
+ 
+    if(elemento.style.display == "" || elemento.style.display == "block") {
+        elemento.style.display = "none";
+        enlace.innerHTML = 'Mostrar contenidos';
+    } else {
+        elemento.style.display = "block";
+        enlace.innerHTML = 'Ocultar contenidos';
+    }
+}
