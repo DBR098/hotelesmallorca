2d5e86baeeb64b9cf5bdaf1f246d023e2aae52c3
SATANAZI <dennybarreirorubio@hotmail.com>
2021-04-28 11:29:10 +0200

    a

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,28 @@ $(document).ready(function () {
         return false;
     });
 });
+
+async function getHoteles() {
+    let url = 'js/hoteles.json';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function printHoteles() {
+    let hoteles = await getHoteles();
+    let html = '';
+    hoteles.forEach(hotel => {
+        let htmlSegment = `<div class="hotel">
+                            <img src="${hotel.icones[0]}">
+                            <h2>${hotel.nom}</h2>
+                            <a>${hotel.descripcio}</a>
+                        </div>`;
+        html += htmlSegment;
+    });
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
