41a451ab0f56d3a2a8da2b1c50a4b68d062f7434
SATANAZI <dennybarreirorubio@hotmail.com>
2021-04-28 10:52:19 +0200

    puto

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
-$(document).ready(function(){
-    $('#open').on('click', function(){
+$(document).ready(function () {
+    $('#open').on('click', function () {
         $('#popup').fadeIn('slow');
         $('.popup-overlay').fadeIn('slow');
         $('.popup-overlay').height($(window).height());
         return false;
     });
 
-    $('#close').on('click', function(){
+    $('#close').on('click', function () {
         $('#popup').fadeOut('slow');
         $('.popup-overlay').fadeOut('slow');
         return false;
     });
 });
 
-$(document).ready(function(){
-    $('#open2').on('click', function(){
+$(document).ready(function () {
+    $('#open2').on('click', function () {
         $('#popup2').fadeIn('slow');
         $('.popup2-overlay').fadeIn('slow');
         $('.popup2-overlay').height($(window).height());
         return false;
     });
 
-    $('#close2').on('click', function(){
+    $('#close2').on('click', function () {
         $('#popup2').fadeOut('slow');
         $('.popup2-overlay').fadeOut('slow');
         return false;
