1719636a519e7676b9e07d26ca5b0cd3311b2577
SATANAZI <dennybarreirorubio@hotmail.com>
2021-05-09 18:15:29 +0200

    ULTIMA VERSION

--- js/scripts.js
+++ js/scripts.js
@@ -182,16 +182,15 @@ function pintar(num) {
 }
 
 function muestraOculta(id) {
-    var elemento = document.getElementById('contenidos_' + id);
-    var enlace = document.getElementById('enlace_' + id);
+    var elemento = document.getElementById('resp_' + id);
+
+    $('#preg_'+id+' span').toggleClass("rotate");
 
     if (elemento.style.display == "" || elemento.style.display == "block") {
         elemento.style.display = "none";
-        enlace.innerHTML = 'Mostrar contenidos';
     }
     else {
         elemento.style.display = "block";
-        enlace.innerHTML = 'Ocultar contenidos';
     }
 }
 
